8a03b2bae109e29fe462ec264bca66f227b65868
rbtmrcs <roberto.marcos@tidewise.io>
2021-03-22 09:50:12 -0300

    fix minor errors in Spawn Server unit tests
    [tmp_root_ca] data compression testing code was been called during
    Spawn Server unit tests. I have no idea why, but left commented
    for future proper deletion or correct implementation
    
    [log_transfer_server] net/ftp library requirement was absent, bubt
    now put in place
    
    [plugin.rb] more testing code raising erros during the Spawn Server
    unit tests run, commented for future deletion or correct implemen-
    tation
    
    [test_spawn_server] fixed minor problem of one instance of testing
    having a testfile remain. Added code to delete it at the end of
    said test

--- test/roby_app/spawn_server/test_spawn_server.rb
+++ test/roby_app/spawn_server/test_spawn_server.rb
@@ -92,7 +92,7 @@ describe Syskit::RobyApp::LogTransferServer::SpawnServer do
         it "uploads a file to the server's directory" do
             upload_testfile
             assert File.exist?("#{@temp_serverdir}/testfile"),
-                   "cannot find the expected upload"
+                "cannot find the expected upload"
         end
 
         it "refuses to upload a file that already exists" do
@@ -108,6 +108,7 @@ describe Syskit::RobyApp::LogTransferServer::SpawnServer do
                     ftp.get("#{@temp_serverdir}/testfile")
                 end
             end
+            File.delete("testfile")
         end
     end
 end
